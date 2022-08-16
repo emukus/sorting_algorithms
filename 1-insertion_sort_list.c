445168798169283321748dae8dd0875c2adb9076
emukus <mukuna.eugene@gmail.com>
2022-08-16 20:27:51 +0000

    Corrections

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,20 +2,21 @@
 
 /**
  * swap_nodes - function that swaps two nodes in a
- *	listint_t doubly-linked list
+ *	listint_t doubly-linked list.
  * @h: ptr to the head of the doubly-linked list.
  * @n1: ptr to the first node to swap.
  * @n2: second node to swap.
  */
+
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	(*n1)->nxt = n2->nxt;
-	if (n2->nxt != NULL)
-		n2->nxt->prev = *n1;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
 	n2->prev = (*n1)->prev;
-	n2->nxt = *n1;
+	n2->next = *n1;
 	if ((*n1)->prev != NULL)
-		(*n1)->prev->nxt = n2;
+		(*n1)->prev->next = n2;
 	else
 		*h = n2;
 	(*n1)->prev = n2;
@@ -23,20 +24,21 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 }
 
 /**
- * insertion_sort_list - function that sorts a doubly linked list of integers
- *	using the insertion sort algorithm.
- * @list: A pointer to the head of a doubly-linked list of integers.
+ * insertion_sort_list - function that orts a doubly linked list of
+ *	integers using the insertion sort algorithm.
+ * @list: ptr to the head of a doubly-linked list of integers.
  */
+
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *iter, *insert, *tmp;
 
-	if (list == NULL || *list == NULL || (*list)->nxt == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->nxt; iter != NULL; iter = tmp)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		tmp = iter->nxt;
+		tmp = iter->next;
 		insert = iter->prev;
 		while (insert != NULL && iter->n < insert->n)
 		{
